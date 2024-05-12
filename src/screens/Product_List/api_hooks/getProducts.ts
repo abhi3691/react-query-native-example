61e8b841059b90524fa3266b22b87b4d57636f0c
abhinandvk <abhinandvk41@gmail.com>
2024-05-12 19:24:24 +0530

    updated

--- src/screens/Product_List/api_hooks/getProducts.ts
+++ src/screens/Product_List/api_hooks/getProducts.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const getProducts = async () => {
-  return await axios.get('https://dummyjson.com/products?limit=100');
+  let randomLimit = Math.round(Math.random() * 99);
+  return await axios.get(`https://dummyjson.com/products?limit=${randomLimit}`);
 };
